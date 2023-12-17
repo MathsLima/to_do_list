681d215713b0374e16d820495b1ed630026bd1e6
MathsLima <matheuslima13@hotmail.com>
2023-12-17 17:25:16 -0300

    projectt updates

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name="Tutulo", max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline_at = models.DateTimeField(null=False, blank=False)
+    deadline_at = models.DateTimeField(verbose_name="Prazo", null=False, blank=False)
     finished_at = models.DateTimeField(null=True)
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Todo
 
+"""
 #lista as tarefas cadastradas
 def todo_list(request):
     todos = Todo.objects.all()
     return render(request, "todos/todo_list.html", {"todos": todos})
+"""
 
+class TodoListView(ListView):
+    model = Todo
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline_at"]
+    success_url = reverse_lazy("todo_list")
